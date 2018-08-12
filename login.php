dabdcc93a81498a245bd4187f3b57fde832f5e54
SHRIRAM0509 <41752215+SHRIRAM0509@users.noreply.github.com>
2018-08-12 15:17:44 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE Html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login</title>
+</head>
+<body>
+<?php
+echo "your name is".$_GET['name'];
+?>
+<br>
+<?php
+echo "you are a".$_GET['sex'];
+?>
+</body>
+</html>
